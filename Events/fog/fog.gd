bc39a721dbd450e8f8ad118f8a67dad7df461518
obani <robin@pi-et-ro.net>
2025-08-19 03:12:57 +0200

    Remove addons that are now in the game

--- Events/fog/fog.gd
+++
@@ -1,8 +0,0 @@
-extends Event
-
-func exit():
-	for p in get_children():
-		p.emitting = false
-		
-	debug.dbg("LIFETIME: " + str(get_children()[0].lifetime))
-	get_tree().create_timer(get_children()[0].lifetime).timeout.connect(queue_free)
